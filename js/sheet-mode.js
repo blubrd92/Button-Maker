5186c1d6d203ff204be28ca8895f8cb02e7575af
blubrd92 <javimt8@gmail.com>
2026-03-10 23:22:20 -0700

    refining

--- js/sheet-mode.js
+++ js/sheet-mode.js
@@ -183,7 +183,7 @@ function renderSheetView() {
     '<button class="btn btn-small" id="btn-apply-col" style="display:none;">Apply to Col</button>' +
     '<button class="btn btn-small" id="btn-apply-row" style="display:none;">Apply to Row</button>' +
     '<button class="btn btn-small" id="btn-make-main" style="display:none;">Make Main Design</button>' +
-    '<button class="btn btn-small" id="btn-edit-selected" style="display:none;">Edit in Design</button>' +
+    '<button class="btn btn-small" id="btn-edit-selected" style="display:none;">Edit</button>' +
     '<button class="btn btn-small" id="btn-copy-design" style="display:none;">Copy</button>' +
     '<button class="btn btn-small" id="btn-paste-design" style="display:none;">Paste</button>' +
     '<button class="btn btn-small" id="btn-clear-selection" style="display:none;">Clear Selection</button>';
